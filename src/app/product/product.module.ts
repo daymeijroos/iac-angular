775a681335529431aaef7e98634a349638888085
Day Meijroos <pekieend@gmail.com>
2022-12-21 16:27:59 +0100

    Fixed imports

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ProductService } from './service/product.service';
 import { __exportStar } from 'tslib';
 
 
